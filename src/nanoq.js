7a03e60c062557117e7c62af1ea7fd164f103fa4
Mark S. Miller <erights@gmail.com>
2017-08-25 08:21:54 -0700

    compat enough with makeQ.js that contract.html works

--- src/nanoq.js
+++ src/nanoq.js
@@ -1,3 +1,5 @@
+// Shim a small subset of Q
+
 (function(global) {
   "use strict";
 
@@ -5,26 +7,19 @@
   const localRelay = {
     GET(p, key) { return p.then(o => o[key]); },
     PUT(p, key, val) { return p.then(o => o[key] = val); },
-    DEL(p, key) { return p.then(o => delete o[key]); },
-    POST(p, key, args) { return p.then(o => o[key](...args)); },
-    FAPPLY(p, args) { return p.then(o => o(...args)); }
+    DELETE(p, key) { return p.then(o => delete o[key]); },
+    POST(p, opt_key, args) {
+      if (opt_key === void 0 || opt_key === null) {
+        return p.then(o => o(...args));
+      } else {
+        return p.then(o => o[opt_key](...args));
+      }
+    }
   };
   
   const relayToPromise = new WeakMap();
   const promiseToRelay = new WeakMap();
   
-  function registerRemote(remoteRelay) {
-    const promise = Promise.resolve(remoteRelay.AWAIT_FAR());
-    relayToPromise.set(remoteRelay, promise);
-    promiseToRelay.set(promise, remoteRelay);
-  }
-  
-  function registerFar(farRelay) {
-    const promise = Promise.resolve(farRelay);
-    relayToPromise.set(farRelay, promise);
-    promiseToRelay.set(promise, farRelay);
-  }
-
   function relay(p) {
     return promiseToRelay.get(p) || localRelay;
   }
@@ -33,21 +28,30 @@
     return relayToPromise.get(specimen) || Promise.resolve(specimen);
   }
   
-  Q.__proto__ = Promise;
-  
   Object.defineProperties(Promise.prototype, Object.getOwnPropertyDescriptors({
     get(key) { return relay(this).GET(this, key); },
     put(key, val) { return relay(this).PUT(this, key, val); },
-    del(key) { return relay(this).DEL(this, key); },
-    post(key, args) { return relay(this).POST(this, key, args); },
-    invoke(key, ...args) { return relay(this).POST(this, key, args); },
-    fapply(args) { return relay(this).FAPPLY(this, args); },
-    fcall(...args) { return relay(this).FAPPLY(this, args); }
+    del(key) { return relay(this).DELETE(this, key); },
+    post(opt_key, args) { return relay(this).POST(this, opt_key, args); },
+    invoke(opt_key, ...args) { return relay(this).POST(this, opt_key, args); },
+    fapply(args) { return relay(this).POST(this, void 0, args); },
+    fcall(...args) { return relay(this).POST(this, void 0, args); }
   }));
+  // Temporary compat with the old makeQ.js
+  Promise.prototype.send = Promise.prototype.invoke;
+  Promise.prototype.delete = Promise.prototype.del;
+  Promise.prototype.end = function() {
+    this.then(_ => {}, reason => { throw reason; });
+  };
 
   const passByCopyRecords = new WeakSet();
 
   Object.defineProperties(Q, Object.getOwnPropertyDescriptors({
+    all: Promise.all.bind(Promise),
+    race: Promise.race.bind(Promise),
+    reject: Promise.reject.bind(Promise),
+    resolve: Promise.resolve.bind(Promise),
+    
     join(p, q) {
       if (Object.is(p, q)) {
         // When p is a pipeline-able promise, this shortcut preserves
@@ -76,11 +80,56 @@
       }
       passByCopyRecords.add(record);
       return record;
+    },
+    makeRemote(remoteRelay, nextSlotP) {
+      // TODO: Use nextSlotP instead of AWAIT_FAR
+      const promise = Promise.resolve(remoteRelay.AWAIT_FAR());
+      relayToPromise.set(remoteRelay, promise);
+      promiseToRelay.set(promise, remoteRelay);
+      return promise;
+    },
+    makeFar(farRelay, nextSlotP) {
+      // TODO: Use nextSlotP to indicate partition breakage
+      const promise = Promise.resolve(farRelay);
+      relayToPromise.set(farRelay, promise);
+      promiseToRelay.set(promise, farRelay);
+      return promise;
+    },
+
+    // Temporary compat with the old makeQ.js
+    // shorten
+    // isPromise
+    // async
+    rejected: Q.reject,
+    promise(func) { return new Promise(func); },
+    delay(millis, opt_answer) {
+      return new Promise(resolve => {
+        setTimeout(() => { resolve(opt_answer); }, millis);
+      });
+    },
+    memoize(oneArgFuncP, opt_memoMap) {
+      var memoMap = opt_memoMap || new WeakMap();
+
+      function oneArgMemo(arg) {
+        var resultP = memoMap.get(arg);
+        if (!resultP) {
+          resultP = Q(oneArgFuncP).fcall(arg);
+          memoMap.set(arg, resultP);
+        }
+        return resultP;
+      }
+      return cajaVM.constFunc(oneArgMemo);
+    },
+    defer() {
+      var deferred = {};
+      deferred.promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+      });
+      return deferred;
     }
   }));
 
   global.Q = Q;
-  global.registerRemote = registerRemote;
-  global.registerFar = registerFar;
   
 })(this);
